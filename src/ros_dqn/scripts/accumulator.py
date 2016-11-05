9c86ce48aef8c7f68b6b139af13a8db3693b02ea
iwawomaru <mosawa@ailab.ics.keio.ac.jp>
2016-11-05 16:34:47 +0900

    improve accumulator

--- src/ros_dqn/scripts/accumulator.py
+++ src/ros_dqn/scripts/accumulator.py
@@ -19,16 +19,19 @@ ga = GazeboAction()
 class Accumulator(object):
     def __init__(self):
         self.accumulators = [0, 0, 0, 0, 0]
+        self.act_id = 0
 
     def __call__(self, message):
-        self.accumulators[message.data] += 0.2
+        self.accumulators[message.data] += 0.5
         rospy.loginfo("Message : %s", str(message.data))
-        for id, accum in enumerate(self.accumulators):
-            if accum >= 1.0:
-                self.accumulators = [0, 0, 0, 0, 0]
-                ga.control_action(id)
-                break
+        if self.accumulators[message.data] >= 1.0:
+            self.accumulators = [0, 0, 0, 0, 0]
+            self.act_id = message.data
             
 accumulator = Accumulator()
 sub = rospy.Subscriber("rand_act", Int16, accumulator)
-rospy.spin()
+
+rate = rospy.Rate(3)
+while not rospy.is_shutdown():
+    ga.control_action(accumulator.act_id)
+    rate.sleep()
